b1809bc91598b97d6ff157ad1dd0ef883cf7a289
malik <malik446644@gmail.com>
2022-05-21 12:39:56 +0200

    changed the background color

--- src/lib/Canvas3D.svelte
+++ src/lib/Canvas3D.svelte
@@ -2,10 +2,12 @@
     import * as THREE from 'three';
 
     const scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xeeeeee );
 
     const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
     const renderer = new THREE.WebGLRenderer();
+    // renderer.setClearColor( 0x555555, 1);
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
 
