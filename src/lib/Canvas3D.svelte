f95660ef7588101addfb4c4636df6f2becce8d38
malik <malik446644@gmail.com>
2022-05-22 12:21:32 +0200

    added a cube

--- src/lib/Canvas3D.svelte
+++ src/lib/Canvas3D.svelte
@@ -7,14 +7,20 @@
     const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
     const renderer = new THREE.WebGLRenderer();
-    // renderer.setClearColor( 0x555555, 1);
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
 
     const geometry = new THREE.BoxGeometry();
     const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    material.wireframe = true
+    material.wireframeLinewidth = 250
     const cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
+    
+    const material2 = new THREE.MeshBasicMaterial( { color: 0x000000 } );
+    // material2.wireframe = true
+    const cube2 = new THREE.Mesh( geometry, material2 );
+    scene.add( cube2 );
 
     camera.position.z = 5;
 
@@ -23,6 +29,9 @@
 
         cube.rotation.x += 0.01;
         cube.rotation.y += 0.01;
+
+        cube2.rotation.x += 0.01;
+        cube2.rotation.y += 0.01;
 
         renderer.render( scene, camera );
     };
